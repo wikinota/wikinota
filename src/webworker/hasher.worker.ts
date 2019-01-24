7cb09e4548e948491556b3c2d2465f687948cb9e
i5heu <gitlab@i5h.eu>
2019-01-24 08:52:02 +0100

    change hash strength

--- src/webworker/hasher.worker.ts
+++ src/webworker/hasher.worker.ts
@@ -3,6 +3,6 @@ const forge = require('node-forge');
 onmessage = function (e) {
 	const email = e.data[0];
 	const pw = e.data[1];
-	const hash = forge.pkcs5.pbkdf2(pw, email, 1000, 512);
+	const hash = forge.pkcs5.pbkdf2(pw, email, 25000, 512);
 	postMessage(forge.util.encode64(hash));
 };
