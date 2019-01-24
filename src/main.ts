01543ac5461abda4466eb530ab596c2a33cf8406
i5heu <gitlab@i5h.eu>
2019-01-24 08:35:40 +0100

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@
 
 import loginCom from "components/login";
 import headerCom from "components/header";
-console.log(loginCom);
 
 customElements.define('login-com', loginCom);
 customElements.define('header-com', headerCom);
