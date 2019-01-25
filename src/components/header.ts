71501728951be3919eea734306c5445135b055b0
i5heu <gitlab@i5h.eu>
2019-01-25 08:43:50 +0100

    add searchbar ui logic

--- src/components/header.ts
+++ src/components/header.ts
@@ -16,17 +16,50 @@ export default class HeaderCom extends HTMLElement {
                 padding:0 5px;
             }
             span {
+                width: auto;
+                display: inline-block;
                 height: 100%;
                 text-align: center;
                 line-height: 35px;
                 font-size: 20px;
+                padding:0;
+                margin: 0;
+            }
+            #searchBar input,
+            #searchBar .fa-times {
+                display: none;
+            }
+            #searchBar.open input {
+                display: inline-block;
+                height: 100%;
+                padding: 0;
+                margin: 0;
+                border: 0;
+            }
+            #searchBar.open .fa-search {
+                display: inline-block;
+            }
+            #searchBar.open .fa-times {
+                display: inline-block;
+                transform: translateX(-120%);
             }
         `;
 
         shadowRoot.innerHTML = `
             <style>${styles}</style>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-            <header><span><i class="fas fa-search"></i></span></header>
+            <header><span id="searchBar"><input></input><i id="searchBarCloseButton" class="fas fa-times"></i><i id="searchBarButton" class="fas fa-search"></i></span></header>
         `;
+
+        const searchBar = shadowRoot.getElementById("searchBar");
+        const searchBarButton = shadowRoot.getElementById("searchBarButton");
+        searchBarButton.onclick = () => {
+            searchBar.classList.add("open");
+        }
+
+        const searchBarCloseButton = shadowRoot.getElementById("searchBarCloseButton");
+        searchBarCloseButton.onclick = () => {
+            searchBar.classList.remove("open");
+        }
     }
 }
