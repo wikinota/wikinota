def9c49584109451829eacdb049ae62800921788
i5heu <gitlab@i5h.eu>
2019-01-26 08:57:02 +0100

    change fontAwesome CDN to one much better

---
+++ src/components/default.ts
@@ -0,0 +1,28 @@
+import { cStyle } from "logic/CustomStyleIO";
+
+export default class DEFAULT extends HTMLElement {
+    shadowRootDom: ShadowRoot = undefined
+
+    constructor() {
+        super();
+        this.shadowRootDom = this.attachShadow({ mode: 'open' });
+        this.newRender();
+    }
+
+    get composedStyle() {
+        let style = `
+        `;
+
+        return style + "\n" + cStyle.header;
+    }
+
+    newRender() {
+        const shadowRoot = this.shadowRootDom
+
+        shadowRoot.innerHTML = `
+        <style>${this.composedStyle}</style>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.6.3/css/all.min.css" integrity="sha384-0t/JV0VqVTwxLAiMN7InD2kF+hreM+s1FynETAE/d21qGK7DuTjZGJ+QTB3BDCV/" crossorigin="anonymous">
+        <header><span id="searchBar"><input></input><i id="searchBarCloseButton" class="fas fa-times"></i><i id="searchBarButton" class="fas fa-search"></i></span></header>
+        `;
+    }
+}
