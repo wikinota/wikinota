2c243a451b8af001933321edb1bc1c836dc0b8bf
i5heu <gitlab@i5h.eu>
2019-01-30 22:33:32 +0100

    add modal

--- src/components/Dashboard.ts
+++ src/components/Dashboard.ts
@@ -4,6 +4,7 @@ import PutTestDataIntoDB from "components/PutTestDataIntoDB";
 import loginCom from "components/login";
 import InputTextContent from "components/InputTextContent";
 import DBHistory from "components/DBHistory";
+import Modal from "components/modal";
 
 export default class Dashboard extends HTMLElement {
     shadowRootDom: ShadowRoot = undefined
@@ -12,6 +13,7 @@ export default class Dashboard extends HTMLElement {
         super();
         this.shadowRootDom = this.attachShadow({ mode: 'open' });
         this.newRender();
+        this.addEventListenerForShadowDom();
     }
 
     get composedStyle() {
@@ -26,15 +28,28 @@ export default class Dashboard extends HTMLElement {
         shadowRoot.innerHTML = `
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.6.3/css/all.min.css" integrity="sha384-0t/JV0VqVTwxLAiMN7InD2kF+hreM+s1FynETAE/d21qGK7DuTjZGJ+QTB3BDCV/" crossorigin="anonymous">
         <style>${this.composedStyle}</style>
+        <button id="openModal">OpenModal</button>
         <puttestdataintodb-com></puttestdataintodb-com>
         <login-com></login-com>
         <inputtextcontent-com></inputtextcontent-com>
         <dbhistory-com></dbhistory-com>
+        <modal-com id="modal" hidden>
+            Hellow!! this is from Slot!
+        </modal-com>
         `;
 
         customElements.define('login-com', loginCom);
         customElements.define('puttestdataintodb-com', PutTestDataIntoDB);
         customElements.define('inputtextcontent-com', InputTextContent);
         customElements.define('dbhistory-com', DBHistory);
+        customElements.define('modal-com', Modal);
+    }
+
+    addEventListenerForShadowDom() {
+        const el = this.shadowRoot.getElementById("openModal");
+        el.onclick = (ev) => {
+            const modal = this.shadowRoot.getElementById("modal");
+            modal.removeAttribute("hidden");
+        }
     }
 }
