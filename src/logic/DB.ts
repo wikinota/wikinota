28a69d78537bfb383661c8623ad89a2c9a390ea9
i5heu <gitlab@i5h.eu>
2019-02-02 23:41:47 +0100

    add sync

--- src/logic/DB.ts
+++ src/logic/DB.ts
@@ -11,6 +11,29 @@ class DB {
         if (pouchdDBSession == undefined) {
             console.info("creating new PouchDB Session")
             pouchdDBSession = new PouchDB('documentStore');
+            pouchdDBSessionSync = PouchDB.sync('documentStore', 'http://XXXXXXX:5984/wikinota_demo', {
+                live: true,
+                retry: true
+            }).on('change', function (info: any) {
+                // handle change
+                console.debug("info", info)
+            }).on('paused', function (err: any) {
+                // replication paused (e.g. replication up to date, user went offline)
+                console.debug("err", err)
+            }).on('active', function () {
+                console.debug("active")
+                // replicate resumed (e.g. new changes replicating, user went back online)
+            }).on('denied', function (err: any) {
+                console.debug("err", err)
+                // a document failed to replicate (e.g. due to permissions)
+            }).on('complete', function (info: any) {
+                // handle complete
+                console.debug("info", info)
+            }).on('error', function (err: any) {
+                console.debug("err", err)
+                // handle error
+            });
+
 
             pouchdDBSession.info().then((info) => {
                 console.info("DB INFO:", info);
