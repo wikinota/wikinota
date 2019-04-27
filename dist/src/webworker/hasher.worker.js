977586d7e2555e732d014d8917af59d861776538
i5heu <gitlab@i5h.eu>
2019-04-27 12:25:48 +0200

    remove fail files

--- dist/src/webworker/hasher.worker.js
+++
@@ -1,8 +0,0 @@
-const forge = require('node-forge');
-onmessage = function (e) {
-    const email = e.data[0];
-    const pw = e.data[1];
-    const hash = forge.pkcs5.pbkdf2(pw, email, 25000, 512);
-    postMessage(forge.util.encode64(hash));
-};
-//# sourceMappingURL=hasher.worker.js.map
